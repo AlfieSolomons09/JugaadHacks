41f1d86de8ae6992679edfd5be3719862eeab941
Bharat Choudhary <chetanchoudhary597@gmail.com>
2024-06-01 01:48:29 +0530

    lowercased

--- data_processing.py
+++ data_processing.py
@@ -21,25 +21,11 @@ for index, row in df.iterrows():
     effects = drug_interactions['effect']
 
     for drug, effect in zip(drugs, effects):
-        key = (concatenated_key, drug)
-        value = "UNSAFE"
-        if(effect == "MODERATE" or effect == ""): value = "SAFE"
+        key = (concatenated_key.lower(), drug.lower())
+        value = "unsafe"
+        if effect.lower() in ["moderate", ""]:
+            value = "safe"
         salt_drug_dict[key] = value
-
-
-# def return_effect(salt_interaction, med_name):
-#     example_key = (salt_interaction, med_name)
-
-#     if example_key in salt_drug_dict:
-#         print(f"\nEffect for Salt Intearction {salt_interaction} and Drug {med_name}: {salt_drug_dict[example_key]}")
-#     else:
-#         print("No interaction found")
-
-
-# def get_effect():
-#     salt_interaction = input("Enter medicine1: ")
-#     med_name = input("Enter medicine2: ")
-#     return_effect(salt_interaction, med_name)
 
 
 print(salt_drug_dict)
