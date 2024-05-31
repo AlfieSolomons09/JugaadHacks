4cae08a338ca1783bcdce9c1706742783190b63f
Bharat Choudhary <chetanchoudhary597@gmail.com>
2024-06-01 01:38:50 +0530

    labelled data

--- data_processing.py
+++ data_processing.py
@@ -2,8 +2,6 @@ import pandas as pd
 import json 
 import re
 
-
-# loading csv file
 file_path = 'medicine_data.csv'
 df = pd.read_csv(file_path)
 
@@ -14,26 +12,34 @@ pattern = r'([A-Za-z\s]+)(?=\s*\()'
 for index, row in df.iterrows():
     salt_composition = row['salt_composition']
     drug_interactions = json.loads(row['drug_interactions'])
-    # print(drug_interactions)
 
     salts = re.findall(pattern, salt_composition)
 
     concatenated_key = ' '.join([salt.strip() for salt in salts])
 
-    # print(salts)
-    # for salt in salts:
-        # key = salt.strip()
-        
-    interactions = []
-
     drugs = drug_interactions['drug']
     effects = drug_interactions['effect']
 
-    for drug,effect in zip(drugs, effects):
-            interactions.append((drug, effect))
+    for drug, effect in zip(drugs, effects):
+        key = (concatenated_key, drug)
+        value = "UNSAFE"
+        if(effect == "MODERATE" or effect == ""): value = "SAFE"
+        salt_drug_dict[key] = value
 
-    salt_drug_dict[concatenated_key] = interactions
 
-# print(salt_drug_dict["Insulin Isophane/NPH"])
+# def return_effect(salt_interaction, med_name):
+#     example_key = (salt_interaction, med_name)
+
+#     if example_key in salt_drug_dict:
+#         print(f"\nEffect for Salt Intearction {salt_interaction} and Drug {med_name}: {salt_drug_dict[example_key]}")
+#     else:
+#         print("No interaction found")
+
+
+# def get_effect():
+#     salt_interaction = input("Enter medicine1: ")
+#     med_name = input("Enter medicine2: ")
+#     return_effect(salt_interaction, med_name)
+
+
 print(salt_drug_dict)
-print(salt_drug_dict["Insulin Aspart Insulin Aspart Protamine"])
